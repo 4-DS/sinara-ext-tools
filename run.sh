4d5adaecb01bf1e6e5069f3a6e9d117bce0d80aa
buslovaev_ma <buslovaev_ma@outlook.com>
2023-03-10 05:53:19 +0000

    Updated run.sh

--- run.sh
+++ run.sh
@@ -7,5 +7,7 @@ if [[ $(docker ps -a --filter "status=exited" | grep "$containerName") ]]; then
   else
     if [[ $(docker ps | grep "$containerName") ]]; then
       echo "Your jovyan single use container is already running"
+    fi
+fi
 
 echo "Please, follow the URL http://127.0.0.1:8888/lab to access your jovyan single use, by using CTRL key"
