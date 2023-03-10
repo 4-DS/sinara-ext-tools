4255a0f5b0c9c78a5f69b0ce35291f91c9768450
buslovaev_ma <buslovaev_ma@outlook.com>
2023-03-10 07:04:37 +0000

    Updated stop.sh

--- stop.sh
+++ stop.sh
@@ -1,16 +1,29 @@
+#!/bin/bash
+#set -euxo pipefail
+set -euo pipefail
+# Parse arguments
+while [ $# -gt 0 ]; do
+    if [[ $1 == *"--"* ]]; then
+        v="${1/--/}"
+        declare $v="$2"
+    fi
+    shift
+done
+
 containerName=jovyan-single-use
 
+instanceName="${instanceName:-$containerName}"
  
-if [[ $(docker ps -a --filter "status=exited" | grep "$containerName") ]]; then
+if [[ $(docker ps -a --filter "status=exited" | grep "$instanceName") ]]; then
   
     echo "Your jovyan single use container is found. It is stopped.";
 else
-    if [[ $(docker ps | grep "$containerName") ]]; then
-      echo "Your jovyan single use container is running. Stopping it.."; docker stop $containerName
+    if [[ $(docker ps | grep "$instanceName") ]]; then
+      echo "Your jovyan single use container is running. Stopping it.."; docker stop $instanceName
     fi
 fi
 
-if [[ $(docker ps -a --filter "status=exited" | grep "$containerName") ]]; then
+if [[ $(docker ps -a --filter "status=exited" | grep "$instanceName") ]]; then
     echo "Your jovyan single use container is successfully stopped. "
 else
     echo "Your jovyan single use container is not found."
