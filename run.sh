5f7ee6cd041f6b15a6ad1a655bfce8e9ecd24c92
buslovaev_ma <buslovaev_ma@outlook.com>
2023-03-10 06:34:03 +0000

    Updated run.sh

--- run.sh
+++ run.sh
@@ -19,5 +19,12 @@ fi
 if [[ ! $(docker ps | grep "$containerName") ]]; then
       echo "Your jovyan single use container is not found. Please, create it with 'create.sh' "
 else
+    # fix permissions
+	docker exec -u 0:0 $containerName chown -R jovyan /tmp
+	docker exec -u 0:0 $containerName chown -R jovyan /data
+
+    # clean tmp if exists
+	docker exec -u 0:0 $containerName bash -c 'rm -rf /tmp/*'
+    
     echo "Please, follow the URL http://127.0.0.1:8888/lab to access your jovyan single use, by using CTRL key"
 fi
