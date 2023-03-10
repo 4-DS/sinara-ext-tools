434d667382dd761e9a14068ab5c40c2b9ec95f98
buslovaev_ma <buslovaev_ma@outlook.com>
2023-03-10 15:19:30 +0000

    Updated containerize.sh

---
+++ containerize.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#set -euxo pipefail
+set -euo pipefail
+# Parse arguments
+while [ $# -gt 0 ]; do
+    if [[ $1 == *"--"* ]]; then
+        v="${1/--/}"
+        declare $v="$2"
+    fi
+    shift
+done
+
+containerName=jovyan-single-use
+
+instanceName="${instanceName:-$containerName}"
+
+read -p "Please, enter ENTITY_PATH for your bentoservice:" bentoservicePath 
+
+docker cp $instanceName:/$bentoservicePath .
+
+docker build .
