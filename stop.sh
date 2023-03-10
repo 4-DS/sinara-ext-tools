c4c0b4b18b7a304377299fc63094fb0cacca9635
buslovaev_ma <buslovaev_ma@outlook.com>
2023-03-10 05:53:04 +0000

    Updated stop.sh

--- stop.sh
+++ stop.sh
@@ -1,2 +1,11 @@
 containerName=jovyan-single-use
-docker stop $containerName
+
+ 
+if [[ $(docker ps -a --filter "status=exited" | grep "$containerName") ]]; then
+  
+    echo "Your jovyan single use container is found. It is stopped.";
+  else
+    if [[ $(docker ps | grep "$containerName") ]]; then
+      echo "Your jovyan single use container is running. Stopping it.."; docker stop $containerName
+    fi
+fi
