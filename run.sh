870df56a1f056fb6761136aa46025522b2aade76
buslovaev_ma <buslovaev_ma@outlook.com>
2023-03-10 05:55:35 +0000

    Updated run.sh

--- run.sh
+++ run.sh
@@ -4,10 +4,14 @@ containerName=jovyan-single-use
 if [[ $(docker ps -a --filter "status=exited" | grep "$containerName") ]]; then
   
     echo "Your jovyan single use container is found"; docker start $containerName
-  else
+else
     if [[ $(docker ps | grep "$containerName") ]]; then
       echo "Your jovyan single use container is already running"
     fi
 fi
 
-echo "Please, follow the URL http://127.0.0.1:8888/lab to access your jovyan single use, by using CTRL key"
+if [[ ! $(docker ps | grep "$containerName") ]]; then
+      echo "Your jovyan single use container is not found. Please, create it with 'create.sh' "
+else
+    echo "Please, follow the URL http://127.0.0.1:8888/lab to access your jovyan single use, by using CTRL key"
+fi
