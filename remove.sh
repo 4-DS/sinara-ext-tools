2223891878fa8ef7243fdd06569ea088e8b427e8
buslovaev_ma <buslovaev_ma@outlook.com>
2023-03-10 07:06:09 +0000

    Updated remove.sh

--- remove.sh
+++ remove.sh
@@ -10,20 +10,22 @@ while [ $# -gt 0 ]; do
     shift
 done
 
+containerName=jovyan-single-use
+
+instanceName="${instanceName:-$containerName}"
+
 withVolumes="${withVolumes:-no}"
 
 echo "Please, keep in mind, that 'remove.sh' will not delete any volumes by default."
 echo "To delete volumes use '--withVolumes yes'"
 echo "Your custom folders will never be deleted"
 
-containerName=jovyan-single-use
-
-if [[ $(docker ps -a --filter "status=created" | grep "$containerName") || $(docker ps -a --filter "status=exited" | grep "$containerName") ]]; then
+if [[ $(docker ps -a --filter "status=created" | grep "$instanceName") || $(docker ps -a --filter "status=exited" | grep "$instanceName") ]]; then
   
-    echo "Your jovyan single use container is found. Removing it.."; docker rm -f $containerName
+    echo "Your jovyan single use container is found. Removing it.."; docker rm -f $instanceName
 else
-    if [[ $(docker ps | grep "$containerName") ]]; then
-      echo "Your jovyan single use container is already running. Stopping and removing it.."; docker stop $containerName && docker rm -f $containerName
+    if [[ $(docker ps | grep "$instanceName") ]]; then
+      echo "Your jovyan single use container is already running. Stopping and removing it.."; docker stop $instanceName && docker rm -f $instanceName
     fi
 fi
 
