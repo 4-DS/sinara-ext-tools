fa797a55e1082c27e472480ac066bda1bd5b1815
buslovaev_ma <buslovaev_ma@outlook.com>
2023-03-20 15:44:03 +0300

    add image tag

--- containerize.sh
+++ containerize.sh
@@ -14,7 +14,16 @@ containerName=jovyan-single-use
 
 instanceName="${instanceName:-$containerName}"
 
-read -p "Please, enter ENTITY_PATH for your bentoservice: " bentoservicePath 
+echo "Note, that image tag for your Docker model image will be automatically taken from your step run-id"
+
+read -p "Please, enter ENTITY_PATH for your bentoservice: " bentoservicePath
+defaultImageTag=$(basename $(dirname "$bentoservicePath")) 
+
+read -p "Please, enter Docker registry address for your model image: " dockerRegistry
+read -p "Please, enter your model image name: " modelName
+read -p "Please, enter your model image tag [default=$defaultImageTag]: " modelImageTag
+
+modelImageTag="${modelImageTag:-$defaultImageTag}"
 
 bentoservice_dir="$(basename $bentoservicePath)"
 
@@ -27,4 +36,4 @@ cd $bentoservice_dir
 unzip model.zip
 rm -f _SUCCESS
 rm -f model.zip
-docker build .
+docker build . -t $dockerRegistry/$modelName:$modelImageTag
